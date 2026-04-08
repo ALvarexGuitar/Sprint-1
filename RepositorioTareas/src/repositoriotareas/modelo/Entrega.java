2d0f7b2c8ba2aa6f6ba488e6b75a1d762579f07b
ALvarexGuitar <159080814+ALvarexGuitar@users.noreply.github.com>
2026-04-08 17:45:13 -0400

    Configuracion de VS y driver de PostgreSQL
    Se agrego el JAR de Postgres a la libreria para trabajar con SupaBase en el entorno VS.

--- RepositorioTareas/src/repositoriotareas/modelo/Entrega.java
+++ RepositorioTareas/src/repositoriotareas/modelo/Entrega.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package repositoriotareas.modelo;
 
-/**
- *
- * @author Windows
- */
+
 public class Entrega {
     private int id;
     private int tareaId;
