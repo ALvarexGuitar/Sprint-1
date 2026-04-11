831ecf198d379d20b38e281d0f2d4c2421fff336
pukyu-soft <pukyusoft@gmail.com>
2026-04-11 17:53:20 -0400

    Update CalificacionDAO.java
    agreagacion de logica para califiar manejo de DB

---
+++ RepositorioTareas/src/repositoriotareas/baseDatos/CalificacionDAO.java
@@ -0,0 +1,55 @@
+package repositoriotareas.baseDatos;
+
+
+
+import repositoriotareas.modelo.Calificacion;
+
+import java.sql.*;
+
+public class CalificacionDAO {
+
+    
+
+    public void insertar(Calificacion c) {
+        String sql = "INSERT INTO calificaciones(entrega_id, nota, comentario) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexionBD.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, c.getEntregaId());
+            ps.setDouble(2, c.getNota());
+            ps.setString(3, c.getComentario());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+
+    public Calificacion obtenerPorEntrega(int entregaId) {
+        String sql = "SELECT * FROM calificaciones WHERE entrega_id = ?";
+
+        try (Connection conn = ConexionBD.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, entregaId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Calificacion(
+                        rs.getInt("id"),
+                        entregaId,
+                        rs.getDouble("nota"),
+                        rs.getString("comentario")
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
